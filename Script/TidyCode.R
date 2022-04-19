51c42d3159ff80e358642d1630a90d3155442847
unknown <sabrina.robber@gmail.com>
2022-04-19 03:11:09 +0200

    Plots

--- Script/TidyCode.R
+++ Script/TidyCode.R
@@ -13,19 +13,30 @@ csv_locationid<- read.csv("./Data/Raw/NestLocations.csv", header=T, sep = ",", d
 
 # 2) Tidying:
 
-#create datasets with selected columns: 1) ("Year", "NestID") 2) ("NestID", "Latitude", "Longitude").
+#create datasets with selected columns: 1) ("Year", "NestID", "Mass") 2) ("NestID", "Latitude", "Longitude").
 
 #1
-Year_ID = csv_sampling%>% select(Year, NestID) #Year that a specific nest was observed to be used
+Year_ID_Mass = csv_sampling%>% select(Year, NestID, Mass) #Year that a specific nest was observed to be used
 
 #2
 Location_ID = csv_locationid%>% select(Latitude, Longitude, NestID) #The GIS co-ordinates of each nest
 
-#removing duplicates from year_ID. Since this is absence/presence, only one observation per year is needed.
 
 #Joining dataframe
+Joined = Year_ID_Mass %>% left_join(Location_ID, by = "NestID")
+Joined$Year <- as.factor(Joined$Year)
 
-Joined = Year_ID %>% left_join(Location_ID, by = "NestID")
+Avemass = Joined %>% group_by(Year)%>%summarize(`Average Mass`= mean(Mass,na.rm=TRUE), `Count` = length(Year)) #Variables I need to plot below
 
-#Delete where NA
-CleanedJoined = na.omit(Joined) #11 removed
+# 3) Plotting 
+
+#Plotting 1) the number of tagged chicks per year, and 2) the average chick mass per year
+
+#1
+plot_avemass = plot(Avemass$Year, Avemass$`Average Mass`, xlab = "Year", ylab = "Average Chick Mass (g)", main = "Graph showing average mass of Starling chicks per year (2017 - 2022)")
+
+#2
+plot_count = plot(Avemass$Year, Avemass$Count, xlab = "Year", ylab = "Number of chicks tagged", main = "Graph showing number of tagged Starling chicks per year (2017 - 2022)")
+
+
+
